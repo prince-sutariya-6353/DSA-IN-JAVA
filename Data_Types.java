fd1d18ba87fd49ca9cfeefaab44d278a1f43945e
prince <princegithub6353@gmail.com>
2025-08-03 17:27:40 +0530

    first commit

--- Data_Types.java
+++ Data_Types.java
@@ -1,5 +1,15 @@
 public class Data_Types {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        double b = 3.14;
+        char c = 'A';
+        String d = "hii";
+        boolean e = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
