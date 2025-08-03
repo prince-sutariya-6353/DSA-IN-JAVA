739bee39f5e52c6575f69a3f98e76c07acc80ef1
prince <princegithub6353@gmail.com>
2025-08-03 21:49:00 +0530

    first commit

---
+++ Operators/Operators.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        // Arithmetic operators
+
+        int a = 100 + 50;
+        int b = 100 / 50;
+        int c = 100 * 50;
+        int d = 100 - 50;
+        int e = 100 % 50;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+        // Relational operators
+        // int f = 10 , g = 20;
+
+  }
+}
